39e652bfe73cdbcd81900d33a59fdc23180dc61b
Maria Milkowski <mariamilkowski@marias-mbp.dhcp.nd.edu>
2026-04-27 10:20:53 -0400

    AWS nonlocal issues

--- mysite/settings.py
+++ mysite/settings.py
@@ -201,6 +201,10 @@ AWS_STORAGE_BUCKET_NAME = "prond-uploads-2026"
 
 AWS_S3_REGION_NAME = "us-east-1"
 
+AWS_DEFAULT_ACL = "public-read"
+
+AWS_QUERYSTRING_AUTH = False
+
 AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
